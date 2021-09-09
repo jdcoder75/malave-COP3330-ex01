f9733ac79c6d6a592e21665d7cb84196c450eb9a
jdcoder75 <89999560+jdcoder75@users.noreply.github.com>
2021-09-09 14:27:34 -0400

    Add files via upload

---
+++ malave-COP3330-ex01/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.*;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+public class App {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("What is your name" );
+        String str= sc.nextLine();
+        System.out.println("Hello, " + str + ", nice to meet you!");
+    }
+}
