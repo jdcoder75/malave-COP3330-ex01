26ec0c85a34e3429d95c2ed531f586d2f1080bc3
jdcoder75 <89999560+jdcoder75@users.noreply.github.com>
2021-09-13 17:34:37 -0400

    Update App.java

--- malave-COP3330-ex01/src/main/java/org/example/App.java
+++ malave-COP3330-ex01/src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Jose Malave
  */
 public class App {
     public static void main(String[] args) {
